c15dd02f79d6bacb7f13c81b706236e03e813ce0
Edward Yeh <edward_yeh@gemteks.com>
2017-07-28 09:30:12 +0800

    Revise Calculator class to add more functions.

--- app/src/main/java/com/gemtek/androidunittest/Calculator.java
+++ app/src/main/java/com/gemtek/androidunittest/Calculator.java
@@ -9,4 +9,16 @@ public class Calculator {
     public int add(int one, int another) {
         return one + another;
     }
+
+    public int multiply(int one, int another) {
+        return one * another;
+    }
+
+    public double divide(double dividend, double divisor) {
+        if (divisor == 0) {
+            throw new IllegalArgumentException("Divisor cannot be 0");
+        }
+        return dividend / divisor;
+    }
 }
+
--- app/src/test/java/com/gemtek/androidunittest/CalculatorTest.java
+++ app/src/test/java/com/gemtek/androidunittest/CalculatorTest.java
@@ -7,16 +7,39 @@ package com.gemtek.androidunittest;
 
 import junit.framework.Assert;
 
+import org.junit.Before;
 import org.junit.Test;
 
 public class CalculatorTest {
+    Calculator mCalculator;
+
+    @Before
+    public void setup() {
+        //Arrange
+        mCalculator = new Calculator();
+    }
+
     @Test
-    public void sumOfTwoIntegers() throws Exception {
-        // Arrange
-        Calculator calculator = new Calculator();
-        // Act
-        int sum = calculator.add(1, 2);
-        // Assert
-        Assert.assertEquals(3, sum);
+    public void addTwoIntegers() throws Exception {
+        //Act
+        int expected = 3;
+        int actual = mCalculator.add(1, 2);
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test
+    public void MultiplyTwoIntegers() throws Exception {
+        //Act
+        int expected = 8;
+        int actual = mCalculator.multiply(2, 4);
+        //Assert
+        Assert.assertEquals(expected, actual);
+    }
+
+    @Test(expected = IllegalArgumentException.class)
+    public void divideByZeroThrowException() {
+        //Act
+        mCalculator.divide(4, 0);
     }
 }
