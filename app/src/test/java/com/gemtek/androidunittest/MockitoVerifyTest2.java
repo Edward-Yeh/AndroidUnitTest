e150fcf0617951d5bd1f22dd7878dff111465570
Edward Yeh <edward_yeh@gemteks.com>
2017-07-31 15:49:12 +0800

    Using @Mock annotation to create mock objects.

---
+++ app/src/test/java/com/gemtek/androidunittest/MockitoVerifyTest2.java
@@ -0,0 +1,34 @@
+/*
+ * Copyright©, 2017 Gemtek Technology Co., Ltd.
+ * All rights reserved.
+ */
+
+package com.gemtek.androidunittest;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import java.util.List;
+
+@RunWith(MockitoJUnitRunner.class)
+public class MockitoVerifyTest2 {
+    @Mock
+    private List mMockedList;
+
+    @Test
+    public void testVerifyMethod() {
+        // Act
+        mMockedList.add("one");
+        mMockedList.add("two");
+        mMockedList.clear();
+
+        // Assert
+        Mockito.verify(mMockedList).clear();
+        Mockito.verify(mMockedList, Mockito.times(1)).add("one");
+        Mockito.verify(mMockedList, Mockito.times(2)).add(Mockito.anyString());
+        Mockito.verify(mMockedList, Mockito.never()).add("three");
+    }
+}
